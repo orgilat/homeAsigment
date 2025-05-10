efaf27c5a5b3ff9af21f6fe00e307594cf81a88f
Gilateam1 <orgilat123@gmail.com>
2025-05-10 15:07:32 +0300

    עדכון בדיקות Playwright ו-YML

--- playwright.config.ts
+++ playwright.config.ts
@@ -4,12 +4,12 @@ export default defineConfig({
   testDir: './tests',
   timeout: 280 * 1000,
   use: {
-    headless: true,
+    headless: false,
     storageState: 'LoginAuth.json',  // שימוש ב-cookies ששמרנו ב-global-setup
     screenshot: 'only-on-failure',
     video: { mode: 'on', size: { width: 1281, height: 720 } },
     viewport: { width: 1280, height: 720 },
-    baseURL: 'https://staging.shufersal.co.il',
+    baseURL:  'https://www.shufersal.co.il/online/he/A',
     ignoreHTTPSErrors: true,
   },
   reporter: [
